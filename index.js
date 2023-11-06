f80127523c57a42e37b71d1dfd9470d84a519500
Kaleab Fekade <kaleab83@gmail.com>
2023-11-05 23:29:23 -0500

    promt made parsed ans stored in a array working

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(`Hello`);
+
+
+const userInput = prompt(`Please enter your desired froyo flavors:`);
+userInput.split(",");
+const userInputStored = [userInput];
+
+console.log(userInputStored);

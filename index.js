de240e370c1aac2e3e886716879fda6e62c3ca7f
Kaleab Fekade <kaleab83@gmail.com>
2023-11-06 21:14:25 -0500

    function with object storing properties and values.

--- index.js
+++ index.js
@@ -2,22 +2,27 @@
 /*
 1. Make a prompt that askes the user for a list or there order
 2. Parse the list of flavors
-3.
+3. Stores it in a array
+4. loop through the array vanilla,vanilla,vanilla,strawberry,coffee,coffee
 */
-
-
-
 const userInputs = prompt(`Please enter your desired froyo flavors:`);
 const inputStored = userInputs.split(",");
 
-//for...in gives back properties of the array and object
 for (let i in inputStored) {
-  console.log(inputStored[i]);
+  console.log(` You have ordered: ${inputStored[i]} `);
 } 
-
 console.table(inputStored);
 
 
 
 
+const froyoFlavors = (vanilla,strawberry,coffee) => {
+  let flavors = {
+    vanilla: `vanilla`,
+    strawberry: `strawberry`,
+    coffee: `coffee`
+  };
+  return flavors;
+}
 
+console.log(froyoFlavors(inputStored));
